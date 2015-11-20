1331c008f04b07a60f95cb6aebffa069196c12a8
wakatakeru <shimizu.net421@gmail.com>
2015-11-20 19:44:23 +0900

    .

--- maze.c
+++ maze.c
@@ -2,8 +2,10 @@
 
 enum status {EMP, BLK, ST, GL};
 
+define SIZE 6
+enum status a[SIZE][SIZE] = {{BLK, BLK, BLK, BLK, STE, BLK}}
 
-int main (void)
+  int main(void)
 {
-    
+  a[SIZE][SIZE] = {{}}
 }

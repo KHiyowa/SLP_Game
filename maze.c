28b08a198861fa52e12b8f84042a88136a56770b
moxtsuan <moxtsuan@hogehoge.com>
2015-11-20 19:47:43 +0900

    .

--- maze.c
+++ maze.c
@@ -3,7 +3,12 @@
 enum status {EMP, BLK, ST, GL};
 
 define SIZE 6
-enum status a[SIZE][SIZE] = {{BLK, BLK, BLK, BLK, STE, BLK}}
+enum status a[SIZE][SIZE] = {{BLK, BLK, BLK, BLK, ST, BLK},
+                             {BLK, EMP, EMP, BLK, EMP, BLK},
+                             {BLK, BLK, BLK, BLK, EMP, BLK},
+                             {BLK, EMP, BLK, BLK, EMP, BLK},
+                             {BLK, EMP, BLK, BLK, EMP, BLK},
+                             {BLK, GL, BLK}}
 
   int main(void)
 {
